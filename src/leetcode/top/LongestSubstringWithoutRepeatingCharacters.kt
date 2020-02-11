23e60689e747bc91817cbe0bd28727f45949d7a6
GoldFlsh <Ryan.Z.Khalil@gmail.com>
2020-02-10 19:08:04 -0600

    Cleanup code of LongestSubstringWithoutRepeatingCharacters.kt

--- src/leetcode/top/LongestSubstringWithoutRepeatingCharacters.kt
+++ src/leetcode/top/LongestSubstringWithoutRepeatingCharacters.kt
@@ -4,7 +4,7 @@ import kotlin.math.max
 
 private val charPool: List<Char> = ('a'..'z') + ('A'..'Z')
 
-fun main(args: Array<String>) {
+fun main() {
     val randomString = (1..10)
         .map { kotlin.random.Random.nextInt(0, charPool.size) }
         .asSequence()
@@ -15,26 +15,24 @@ fun main(args: Array<String>) {
 }
 
 class LongestSubstringWithoutRepeatingCharacters {
-    fun lengthOfLongestSubstring(str: String): Int {
-        return when {
+    fun lengthOfLongestSubstring(str: String): Int =
+        when {
             str.length == 1 -> 1
             str.isEmpty() -> 0
             else -> helper(str, 0, 1, 0)
         }
+
+    private tailrec fun helper(
+        str: String,
+        leftIndex: Int,
+        rightIndex: Int,
+        max: Int
+    ): Int = when {
+        rightIndex >= str.length + 1 -> max
+        str.substring(leftIndex, rightIndex).isDistinct() ->
+            helper(str, leftIndex, rightIndex + 1, max(rightIndex - leftIndex, max))
+        else -> helper(str, leftIndex + 1, leftIndex + max + 1, max)
     }
 
-    private tailrec fun helper(str: String, leftIndex: Int, rightIndex: Int, max: Int): Int {
-        return when {
-            rightIndex >= str.length+1 -> max
-            rightIndex >= str.length && isDistinct(str.substring(leftIndex)) ->
-                helper(str, leftIndex, rightIndex + 1, max(rightIndex - leftIndex, max))
-            isDistinct(str.substring(leftIndex, rightIndex)) ->
-                helper(str, leftIndex, rightIndex + 1, max(rightIndex - leftIndex, max))
-            else -> helper(str, leftIndex + 1, leftIndex + max + 1, max)
-        }
-    }
-
-    private fun isDistinct(substring: String): Boolean {
-        return substring.toList().distinct().size == substring.length
-    }
+    private fun String.isDistinct() = toSet().size == length
 }
